2b80b91f16ad0ae7506114d48f434cd07bf41a67
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2018-09-26 21:40:36 +0900

    12.3.2.1 Config のコンストラクタを作成する

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -36,10 +36,11 @@ struct Config {
     filename: String,
 }
 
-// 引数解析器
-fn parse_config(args: &[String]) -> Config {
-    // clone は参照を保持するよりも時間とメモリを消費するが、参照のライフタイムを管理する必要がなくなる
-    let query = &args[1].clone();
-    let filename = &args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // clone は参照を保持するよりも時間とメモリを消費するが、参照のライフタイムを管理する必要がなくなる
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
